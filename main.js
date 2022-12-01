52342000cd4fb1a148b0f759cb14cc1f5f4171c0
AndresF-sanchezG <e.digitalcolombia@gmail.com>
2022-11-30 20:53:22 -0500

    Version 1

---
+++ main.js
@@ -0,0 +1,98 @@
+const botonNumeros = document.querySelectorAll('#button-number');
+const botonOperadores = document.querySelectorAll('#operator');
+const borrarNumeros = document.querySelector('#flecha-button');
+const limpiarboton = document.querySelector('#C-button');
+const limpiarNumeros = document.querySelector('#CE-button');
+let boxNumber = document.querySelector('#box-container');
+
+let arrayNumeros = [];
+let arrayNumerosEditado = [];
+let numerosString;
+let operador;
+let numero1;
+let numero2;
+
+agregarNumeros();
+editarNumeros();
+guardarNumeros();
+limpiar();
+//limpiarNumero();
+
+function agregarNumeros() {
+   botonNumeros.forEach(button=>{
+    button.addEventListener('click',()=>{
+      arrayNumeros.push(button.innerText);
+      numerosString = arrayNumeros.join("");
+      boxNumber.innerText = numerosString;
+    })
+   })
+}
+
+function editarNumeros() {
+  borrarNumeros.addEventListener('click', ()=>{
+    arrayNumeros.pop();
+    numerosString = arrayNumeros.join("");
+    boxNumber.innerText = numerosString;
+  })
+}
+
+function guardarNumeros() {
+ botonOperadores.forEach(button=>{
+  button.addEventListener('click',()=>{
+    if(!numero1) {
+      numero1 = parseInt(numerosString);
+      console.log("numero1=" + numero1);
+      arrayNumeros = [];
+      numerosString = "";
+      resultado = numero1;
+      console.log("Resultado="+resultado);
+      
+    } else {
+      numero2 = parseInt(numerosString);
+      arrayNumeros = [];
+      console.log("numero2=" + numero2);
+    }
+    operaciones();
+    operador = button.innerText; 
+  })
+ })
+}
+
+function operaciones(){
+  if(operador ==="+") {
+    resultado = numero1 + numero2;
+    numero1 = resultado;
+    numero2 = "";
+    boxNumber.innerText = resultado;
+  } 
+  if(operador ==="-") {
+    resultado = numero1 - numero2;
+    numero1 = resultado;
+    numero2 = "";
+    boxNumber.innerText = resultado;
+  } 
+  if(operador ==="*") {
+    resultado = numero1 * numero2;
+    numero1 = resultado;
+    numero2 = "";
+    boxNumber.innerText = resultado;
+  } 
+
+  if(operador ==="/") {
+    resultado = numero1 / numero2;
+    numero1 = resultado;
+    numero2 = "";
+    boxNumber.innerText = resultado;
+  } 
+}
+
+function limpiar() {
+  limpiarboton.addEventListener('click', ()=>{
+    boxNumber.innerText = '';
+    arrayNumeros = [];
+    numerosString = "";
+    resultado = "";
+    numero1 = "";
+    numero2 = "";
+  })
+}
